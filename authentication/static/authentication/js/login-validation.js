8cd3951ee9e76eccad2d22554b9931a35a677593
AntigoniMoira <antmoira@gmail.com>
2019-05-06 09:45:28 +0300

    commit to fix users db

--- authentication/static/authentication/js/login-validation.js
+++ authentication/static/authentication/js/login-validation.js
@@ -9,9 +9,12 @@ const utils = new Utils();
 const loginValidation = function () {
 
     $('.form-signin').submit(function (e) {
-        // catch the form's submit event
-
         e.preventDefault();
+        // catch the form's submit event
+        $('#inputEmail').removeClass('is-invalid');
+        $('#inputPassword').removeClass('is-invalid');
+        utils.hidemsg('#login-fail-message');
+
         var login_data = {
             email: $('#inputEmail').val(),
             password: $('#inputPassword').val()
@@ -22,6 +25,12 @@ const loginValidation = function () {
             if (return_data.success === true) {
                 window.location.href = $("input[name='next']").val() + "&scope=" + return_data.scopes;
             } else if (return_data.success === false) {
+                if (return_data.message.includes('email')){
+                    $('#inputEmail').addClass('is-invalid');
+                }
+                else if(return_data.message.includes('password')){
+                    $('#inputPassword').addClass('is-invalid');
+                }
                 utils.showmsg('#login-fail-message', return_data.message);
             }
         }).catch((error) => {
@@ -34,6 +43,7 @@ const loginValidation = function () {
     $('.form-reset-psw').submit(function (e) {
         // catch the form's submit event
         e.preventDefault();
+        $('#reser-psw-email').removeClass('is-invalid');
         utils.hidemsg('#reset-psw-success-message');
         utils.hidemsg('#reset-psw-fail-message');
         var data = {
@@ -45,6 +55,7 @@ const loginValidation = function () {
             if (return_data.success === true) {
                 utils.showmsg('#reset-psw-success-message', return_data.message);
             } else if (return_data.success === false) {
+                $('#reser-psw-email').addClass('is-invalid');
                 utils.showmsg('#reset-psw-fail-message', return_data.message);
             }
         }).catch((error) => {
